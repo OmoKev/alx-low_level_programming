e032dac32c918ce9d763d7a9c642265f8fd4f30c
OmoKev <ahunsimenkelvin7@gmail.com>
2023-09-07 22:54:36 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,10 @@
 
 /**
  * main - this is the main function
- *
- * Return: 0
+ * Return: always 0
  */
-
 int main(void)
-(
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-)
+}
